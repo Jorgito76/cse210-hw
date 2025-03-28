f1bc591444a5f537397a575dee20af59e3d356ee
Jorgito76 <ova20003@byu.edu>
2025-03-28 09:32:18 -0600

    more scoring stuff and created generating words

---
+++ final/FinalProject/ScoreTimed.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+
+public class ScoreTimed : ScoringRules
+{
+    // Attributes
+    private int score;
+
+    // Constructors
+
+
+    // Methods
+    public override int CalculateScore(int numGuesses, List<string> letters, string word)
+    {
+        int totalScore = 0;
+        return totalScore;
+    }
+    public override void ShowScore()
+    {
+        Console.WriteLine($"Your simple word score: {score} ");
+    }
+
+
+}
